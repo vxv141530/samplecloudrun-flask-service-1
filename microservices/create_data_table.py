8a6a6d0f8f3e3fd71823fe30fa2cbee256d8beee
michelle <michelle.doyle@ascension.org>
2024-09-10 06:11:54 -0500

    update

--- microservices/create_data_table.py
+++ microservices/create_data_table.py
@@ -6,12 +6,14 @@ spanner_app = Flask(__name__)
 
 # Cloud Spanner configurations
 PROJECT_ID = "asc-ahnat-rthe-sandbox-poc"
-INSTANCE_ID = "the-poc1"
+INSTANCE_ID = "the-poc2"
 DATABASE_ID = "rthe-poc"
 
 
 # Function to create the person_patient table
 def create_person_patient_table():
+    from google.cloud.spanner_admin_database_v1.types import \
+        spanner_database_admin
     try:
         spanner_client = spanner.Client()
         instance = spanner_client.instance(INSTANCE_ID)
@@ -37,17 +39,61 @@ def create_person_patient_table():
         print(f"Error creating table: {e}")
         raise
 
+
 @app.route('/create-table', methods=['GET'])
 def create_table():
     try:
-        create_person_patient_table()
+        # create_person_patient_table()
+        create_database(INSTANCE_ID, DATABASE_ID)
         return jsonify({"message": "Table person_patient created successfully!"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
+def create_database(instance_id, database_id):
+    """Creates a database and tables for sample data."""
+    from google.cloud.spanner_admin_database_v1.types import \
+        spanner_database_admin
+
+    spanner_client = spanner.Client()
+    database_admin_api = spanner_client.database_admin_api
+
+    request = spanner_database_admin.CreateDatabaseRequest(
+        parent=database_admin_api.instance_path(spanner_client.project, instance_id),
+        create_statement=f"CREATE DATABASE `{database_id}`",
+        extra_statements=[
+            """CREATE TABLE Encounter (
+            EncounterId     INT64 NOT NULL,
+            FirstName    STRING(1024),
+            LastName     STRING(1024),
+            PersonInfo   BYTES(MAX),
+            FullName   STRING(2048) AS (
+                ARRAY_TO_STRING([FirstName, LastName], " ")
+            ) STORED
+        ) PRIMARY KEY (EncounterId)""",
+            """CREATE TABLE Albums (
+            PartyId     INT64 NOT NULL,
+            PersonId      INT64 NOT NULL
+        ) PRIMARY KEY (EncounterId, PartyId),
+        INTERLEAVE IN PARENT Encounter ON DELETE CASCADE""",
+        ],
+    )
+
+    operation = database_admin_api.create_database(request=request)
+
+    print("Waiting for operation to complete...")
+    database = operation.result(60)
+
+    print(
+        "Created database {} on instance {}".format(
+            database.name,
+            database_admin_api.instance_path(spanner_client.project, instance_id),
+        )
+    )
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    spanner_app.run(debug=True)
     # Ensure the GOOGLE_APPLICATION_CREDENTIALS environment variable is set
     if not os.getenv("GOOGLE_APPLICATION_CREDENTIALS"):
         print("ERROR: The GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.")
