112c3854005dd1584141eeaf11417287a8b31129
michelle <michelle.doyle@ascension.org>
2024-09-10 20:39:17 -0500

    update

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,4 @@ COPY . ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 ENTRYPOINT ["gunicorn"]
-CMD [ "--bind:$PORT","--workers 1", "--threads 8" ,"--timeout 0", "main:app"]
+CMD [ "--bind:$PORT","--workers 1", "--threads 8" ,"--timeout 0", "main:services"]
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import config
 import json
 import time
-import os
-from flask import Flask, jsonify, Response, request
+from flask import jsonify, Response
 from blueprints.activities import activities
-from microservices.create_services import spanner_app
 from microservices.read_services import read_data_from_spanner
-from microservices.create_services import create_table
+from flask import Flask, request
 
 # Cloud Spanner configurations
 PROJECT_ID = "asc-ahnat-rthe-sandbox-poc"
@@ -14,30 +12,30 @@ INSTANCE_ID = "the-poc2"
 DATABASE_ID = "rthe-poc"
 
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(activities, url_prefix="/api/v1/activities")
+def create_services():
+    services = Flask(__name__)
+    services.register_blueprint(activities, url_prefix="/api/v1/activities")
 
     # Error 404 handler
-    @app.errorhandler(404)
+    @services.errorhandler(404)
     def resource_not_found(e):
         return jsonify(error=str(e)), 404
 
     # Error 405 handler
-    @app.errorhandler(405)
+    @services.errorhandler(405)
     def resource_not_found(e):
         return jsonify(error=str(e)), 405
 
     # Error 401 handler
-    @app.errorhandler(401)
+    @services.errorhandler(401)
     def custom_401(error):
         return Response("API Key required.", 401)
 
-    @app.route("/ping")
+    @services.route("/ping")
     def hello_world():
         return "pong"
 
-    @app.route("/read_service/query", methods=["GET"], strict_slashes=False)
+    @services.route("/read_service/query", methods=["GET"], strict_slashes=False)
     def read_service_query():
         instance_id = request.args.get("instance_id")
         database_id = request.args.get("database_id")
@@ -46,21 +44,21 @@ def create_app():
         results = read_data_from_spanner(instance_id, database_id, table_name)
         response_body = {
             "success": 1,
+            "data": results
         }
         return jsonify(response_body)
 
-    @app.route("/version", methods=["GET"], strict_slashes=False)
+    @services.route("/version", methods=["GET"], strict_slashes=False)
     def version():
         response_body = {
             "success": 1,
         }
         return jsonify(response_body)
 
-    @app.after_request
+    @services.after_request
     def after_request(response):
         if response and response.get_json():
             data = response.get_json()
-
             data["time_request"] = int(time.time())
             data["version"] = config.VERSION
 
@@ -68,22 +66,11 @@ def create_app():
 
         return response
 
-    return app
+    return services
 
 
-app = create_app()
-#
-# def create_spanner_table():
-#     spanner_app.create_table()
-#
-#
-# def read_spanner_table():
-#     read_data_from_spanner(INSTANCE_ID, DATABASE_ID, "sample_table")
-
-
-# read = read_service()
+services = create_services()
 
 if __name__ == "__main__":
     print("Starting creating spanner database table...")
-    # create_spanner_table()
-    # read_spanner_table()
+    services.run(debug=True)
